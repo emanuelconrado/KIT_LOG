1d1558fb0e8af074c33ae8ee37cb271c5368a725
emanuelconrado <emanuelthadeu45@gmail.com>
2024-09-07 16:47:07 -0300

    BestImprovementOrOpt: 1 and 2

--- save.cpp
+++ save.cpp
@@ -1,72 +1,8 @@
-#include "Data.h"
-#include <iostream>
-#include <vector>
-#include <time.h>
-#include <stdlib.h>
-#include "Solution.h"
-#include <random>
-#include <algorithm>
-
-using std::vector;
-using std::cout;
-using std::endl;
-
-
-
 struct InsertionInfo{
     int noInserido; // no k a ser inserido
     int arestaRemovida;  // aresta {i,j} na qual o no k sera inserido
     double custo; // delta ao inserir k na aresta {i,j}
 };
-
-void BuscaLocal(Solution *s, Data data);
-Solution Perturbacao(Solution best);
-Solution ILS(int maxItr, int MaxIterIls, Data& data);
-vector<int> escolher3NosAleatorios(Data& data);
-vector<int> nosRestantes(Solution s, Data& data);
-Solution Construcao(Data &data);
-void inserirNaSolução(Solution& s, InsertionInfo noInserido, vector<int>& CL);
-
-
-bool bestImprovementSwap(Solution *s, Data data);
-bool bestImprovement2Opt(Solution *s, Data data);
-bool bestImprovementOrOpt(Solution *s, Data data, int choice)
-
-void calcularCustoInsercao(Solution& s, vector<int>& CL, Data& data, vector<InsertionInfo>& custoInsercao);
-void ordenarEmOrdemCrescente(vector<InsertionInfo>& custoInsercao);
-
-int main(int argc, char** argv) {
-
-    auto data = Data(argc, argv[1]);
-    data.read();
-    size_t n = data.getDimension();
-
-    Construcao(data);
-
-    /*
-    cout << "Dimension: " << n << endl;
-    cout << "DistanceMatrix: " << endl;
-    data.printMatrixDist();
-
-    cout << "Exemplo de Solucao s = ";
-    double cost = 0.0;
-    for (size_t i = 1; i < n; i++) {
-        cout << i << " -> ";
-        cost += data.getDistance(i, i+1);
-    }
-    */
-
-    /*
-    cost += data.getDistance(n, 1);
-    cout << n << " -> " << 1 << endl;
-    cout << "Custo de S: " << cost << endl;
-    */
-
-    return 0;
-}
-
-
-//Implementação das funções:
 
 //Busca pela melhor solução dado uma vizinhança
 
@@ -79,18 +15,6 @@ void BuscaLocal(Solution *s, Data data){
         switch(NL[n]){
         case 1:
             improved = bestImprovementSwap(s, data);
-            break;
-        case 2:
-            improved = bestImprovement2Opt(s, data);
-            break;
-        case 3:
-            improved = bestImprovementOrOpt(s, data, 1);
-            break;
-        case 4:
-            improved = bestImprovementOrOpt(s, data, 2);
-            break;
-        case 5:
-            improved = bestImprovementOrOpt(s, data, 3);
             break;
         }
 
@@ -251,7 +175,7 @@ bool bestImprovementSwap(Solution *s, Data data){
     double bestDelta = 0;
     int best_i, best_j;
 
-    for(int i = 1; i < s->sequence.size() - 2; i++){
+    for(int i = 1; i < s->sequence.size() - 1; i++){
         int vi = s->sequence[i];
         int vi_next = s->sequence[i+1];
         int vi_prev = s->sequence[i-1];
@@ -291,115 +215,4 @@ bool bestImprovementSwap(Solution *s, Data data){
     }
 
     return false;
-}
-
-bool bestImprovement2Opt(Solution *s, Data data){
-    double bestDelta = 0;
-    int best_i, best_j;
-
-    for(int i = 1; i < s->sequence.size() - 3; i++){
-        int vi = s->sequence[i];
-        int vi_next = s->sequence[i+1];
-        int vi_prev = s->sequence[i-1];
-
-        for(int j = i+2; j < s->sequence.size() - 1; j++){
-            int vj = s->sequence[j];
-            int vj_next = s->sequence[j+1];
-            int vj_prev = s->sequence[j-1];
-
-            double delta = - data.getDistance(vi_prev, vi) - data.getDistance(vj, vj_next)
-            + data.getDistance(vi_prev, vj) + data.getDistance(vi, vj_next);
-
-    
-            if (delta < bestDelta)
-            {
-                bestDelta = delta;
-                best_i = i;
-                best_j = j;
-            }
-        }
-    }
-
-    if(bestDelta < 0){
-        cout << s->sequence[best_i] << " e " << s->sequence[best_j] << endl;
-        swap(s->sequence[best_i], s->sequence[best_j]);
-        s->cost = s->cost + bestDelta;
-        return true;
-    }
-}
-
-bool bestImprovementOrOpt(Solution *s, Data data, int choice){
-    double bestDelta = 0;
-    int best_i, best_j;
-
-    switch (choice){
-    case 1:
-        for(int i = 0; i < s->sequence.size() - 1; i++){
-        int vi = s->sequence[i];
-        int vi_next = s->sequence[i+1];
-
-            for(int j = 1; j < s->sequence.size() - 2; j++){
-                int vj = s->sequence[j];
-                int vj_next = s->sequence[j+1];
-                int vj_prev = s->sequence[j-1];
-
-                if(vi == vj || vi_next == vj){
-                    continue;
-                }
-
-                double delta = - data.getDistance(vi, vi_next) + data.getDistance(vi, vj)
-                + data.getDistance(vj, vi_next) - data.getDistance(vj_prev, vj)
-                - data.getDistance(vj, vj_next) + data.getDistance(vj_prev, vj_next); 
-
-    
-                if (delta < bestDelta)
-                {
-                bestDelta = delta;
-                best_i = i;
-                best_j = j;
-                }
-            }
-        }
-        break;
-
-    case 2:
-        for(int i = 0; i < s->sequence.size() - 1; i++){
-        int vi = s->sequence[i];
-        int vi_next = s->sequence[i+1];
-
-            for(int j = 1; j < s->sequence.size() - 2; j++){
-                int vj1 = s->sequence[j];
-                int vj2 = s->sequence[j+1];
-                int vj_next = s->sequence[j+2]
-                int vj_prev = s->sequence[j-1];
-
-                if(vi == vj1 || vi == vj2 || vi_next == vj1){
-                    continue;
-                }
-
-                double delta = - data.getDistance(vi, vi_next) + data.getDistance(vi, vj)
-                + data.getDistance(vj, vi_next) - data.getDistance(vj_prev, vj)
-                - data.getDistance(vj, vj_next) + data.getDistance(vj_prev, vj_next); 
-
-    
-                if (delta < bestDelta)
-                {
-                bestDelta = delta;
-                best_i = i;
-                best_j = j;
-                }
-            }
-        }
-        break;
-
-    case 3:
-        break;
-    }
-
-    if(bestDelta < 0){
-        cout << s->sequence[best_i] << " e " << s->sequence[best_j] << endl;
-        swap(s->sequence[best_i], s->sequence[best_j]);
-        s->cost = s->cost + bestDelta;
-        return true;
-    }
 }
